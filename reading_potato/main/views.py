f66d767d79cc8a3d08f257fc03f6db1c60ef6fd0
Quiet <aquietstranger.gaming+git@gmail.com>
2021-03-21 19:56:19 -0500

    List Of Articles > View

--- reading_potato/main/views.py
+++ reading_potato/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+
+def articles_list(request):
+    # Whatever logic we need to write in our view.
+    articles = Article.objects.all()
+    context = {
+        "articles" : articles,
+    }
+    return render(request, "articles_list.html", context)
+
